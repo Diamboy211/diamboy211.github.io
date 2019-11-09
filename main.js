3871b0eb24d6e5faa33321cfe8845a72ec82b788
Diamboy211 <51283163+Diamboy211@users.noreply.github.com>
2019-11-09 20:39:56 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
-var clicks = 0
+var clicks = [0,0,0,0,0,0,0,0,0,0,0,0,0]
 
 function clickss() {
-  clicks += 1;
-  document.getElementById("clickss").innerHTML = clicks
+  for (let i = 0; i < clicks.length-1; i++) {
+    clicks[i] += clicks[i+1]
+  }
+  clicks[clicks.length-1]++
+  document.getElementById("clickss").innerHTML = clicks[0]
 }
